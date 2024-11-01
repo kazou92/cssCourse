9b835204f2b4743b4e2e988b3e96065d159e6d10
MACPRO <macpro@MACPROs-MacBook-Pro.local>
2024-11-01 20:20:15 -0300

    mi primer git aprediendo css

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log('hola mundo')
+
+/* setInterval(() => {
+  // console.log('hola mundo')
+  const title = document.querySelector(".hero_title");
+
+  if (title.style.color == "red") {
+    title.style.color = "green";
+  } else {
+    title.style.color = "red";
+  }
+}, 1000);
+
+const btn = document.querySelector(".btn_cti");
+
+const nav = document.querySelector(".nav_list");
+
+const links = document.querySelectorAll(".nav_link");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("nav-links_show");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("nav-links_show");
+  });
+}); */
+
+// contexto de this
